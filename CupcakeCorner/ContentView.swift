d78936ca7ffd94dfc7d1c8aeb5c80bf6216ac37b
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-01 00:43:06 -0300

    Rollback

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,48 +7,10 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
-struct ContentView: View {
-    @State private var results = [Result]()
-    
+struct ContentView: View {  
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()
-        }
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=cage+the+elephant&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedRespnse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedRespnse.results
-            }
-        } catch {
-            print("Invalid datas")
-        }
-        
+        Text("Hello, world!")
+            .padding()
     }
 }
 
