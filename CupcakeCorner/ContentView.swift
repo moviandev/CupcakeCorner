49cdc8418ccd17e327deb001c13f21274b0e25ac
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-01 01:13:07 -0300

    Rollback

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,27 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
-    
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Creating account")
-                }
-            }
-            .disabled(disableForm)
-        }
-    }
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
+        Text("Hello, world!")
+            .padding()
     }
 }
 
