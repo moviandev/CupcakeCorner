2b98e55376f1868a07ece178a5b012e33fe9d035
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-01 00:20:36 -0300

    Rollback

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,25 +7,6 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Matheus Viana"
-    
-    // Better than UserDefaults
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-}
-
 struct ContentView: View {
     var body: some View {
         VStack {
