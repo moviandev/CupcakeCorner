f90c9326a768ae9e8ccb96cab26911782b3b8612
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-01 01:12:41 -0300

    Disable form when properties values doesn't conform to validation

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account")
+                }
+            }
+            .disabled(disableForm)
+        }
+    }
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
 }
 
