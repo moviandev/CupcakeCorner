a044126d0e86df7554717bd856ccc4f4f73a0f3c
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-01 00:07:27 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Matheus Viana on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
