9e4b972de6d0d4dc4e6a75532754516ccc0b2100
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-01 23:37:32 -0300

    Create NavigationView with a form with a picker to select the flavor of the cake and the number of cakes the user want

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -11,8 +11,20 @@ struct ContentView: View {
     @StateObject var order = Order()
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
     }
 }
 
