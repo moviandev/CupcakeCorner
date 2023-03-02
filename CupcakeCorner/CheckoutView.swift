78bbef7e09dd0566f26a1145d6004e90c9dafb75
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-02 00:13:01 -0300

    Create computed property to calculate cake cost

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
